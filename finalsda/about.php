1c3ff3309ab6fdb5b2c1ad93d6c7d3c17afeb48e
NahdaRizky18 <64570718+NahdaRizky18@users.noreply.github.com>
2020-06-06 19:32:39 +0700

    Add files via upload

---
+++ finalsda/about.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <meta charset="utf-8">
+                <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+                    <link href="assets\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+                        <link href="assets\css\fontawesome-free\css\all.min.css" rel="stylesheet">
+                            <link href="assets\css\style.css" rel="stylesheet">
+                                <title>
+                                    Data Editor
+                                </title>
+                            </link>
+                        </link>
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <a class="navbar-brand font-weight" href="index.php">
+                <i class="fas fa-edit">
+                </i>
+                DATA EDITOR
+            </a>
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="cari.php">
+                            PENCARIAN EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="tampil.php">
+                            TAMPILKAN EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="about.php">
+                            ABOUT ME
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="container mt-5">
+        <center>
+        <html>
+        <head>
+        <style>
+            table {
+            border-collapse: collapse;
+            width:35%;
+            }
+
+            th, td {
+            padding: 15px;
+            text-align: left;
+            border: 1px solid #ddd;
+            }
+        </style>
+        </head>
+        <body>
+            <table>
+            <tr>
+                <td>Nama</td>
+                <td>Nahda Rizky</td>
+            </tr>
+            <tr>
+                <td>TTL</td>
+                <td>Kota Bakti, 22 November 2000</td>
+            </tr>
+            <tr>
+                <td>Status</td>
+                <td>Mahasiswa</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>S1 Informatika Universitas Syiah Kuala</td>
+            </tr>
+            </table>
+        </body>
+        </html>
+        </br>
+        <img src="assets\\img\\nahda.jpeg" width="30%">
+        </center>
+        </div>
+        <script src="assets\bootstrap\js\jquery.min.js">
+        </script>
+        <script src="assets\bootstrap\js\bootstrap.min.js">
+        </script>
+    </body>
+</html>
---
+++ finalsda/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+	<link rel="stylesheet" href="assets\css\fontawesome-free\css\all.min.css">
+	<link rel="stylesheet" href="assets\css\style.css">
+	<title>Data Editor</title>
+</head>
+
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+		<a class="navbar-brand font-weight" href="#"><i class="fas fa-edit"></i> DATA EDITOR</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="cari.php">PENCARIAN EDITOR</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="tampil.php">TAMPILKAN EDITOR</a>
+				</li>
+                <li class="nav-item active">
+				<a class="nav-link" href="about.php">ABOUT ME</a>
+			</li>
+			</ul>
+		</div>
+	</nav>
+	<!-- End Navbar -->
+
+
+
+	<script src="assets\bootstrap\js\jquery.min.js"></script>
+	<script src="assets\bootstrap\js\bootstrap.min.js"></script>
+
+</body>
+
+</html>
